62d52526089765e3d5ed4d83230b9be518f2aed8
JamieW87 <jamie.w101@gmail.com>
2023-01-08 23:54:23 +0000

    unit tests

--- src/main/java/org/deblock/exercise/controller/DeblockFlightsController.java
+++ src/main/java/org/deblock/exercise/controller/DeblockFlightsController.java
@@ -1,6 +1,5 @@
 package org.deblock.exercise.controller;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.deblock.exercise.model.rest.DeblockRequest;
@@ -12,8 +11,6 @@ import org.deblock.exercise.service.DeblocksFlightsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping(value = "/flights")
--- src/test/java/org/deblock/exercise/controller/DeblocksFlightsControllerTest.java
+++ src/test/java/org/deblock/exercise/controller/DeblocksFlightsControllerTest.java
@@ -1,4 +1,36 @@
 package org.deblock.exercise.controller;
 
+import org.deblock.exercise.model.rest.DeblockRequest;
+import org.deblock.exercise.model.rest.DeblockResponse;
+import org.deblock.exercise.service.DeblocksFlightsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class DeblocksFlightsControllerTest {
+
+    @Mock
+    private DeblocksFlightsService service;
+
+    @InjectMocks
+    private DeblockFlightsController controller;
+
+    @Test
+    public void testFetchFlights() {
+        DeblockRequest deblockRequest = new DeblockRequest();
+        DeblockResponse[] expectedResponse = new DeblockResponse[0];
+        when(service.FetchFlightData(deblockRequest)).thenReturn(expectedResponse);
+
+        ResponseEntity<DeblockResponse[]> actualResponse = controller.fetchFlights(deblockRequest);
+
+        assertEquals(expectedResponse, actualResponse.getBody());
+        assertEquals(200, actualResponse.getStatusCodeValue());
+    }
 }
