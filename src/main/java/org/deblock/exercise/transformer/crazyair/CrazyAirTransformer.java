36b5f09120c2b3e05eb66d32ef675eda5d4667ba
JamieW87 <jamie.w101@gmail.com>
2023-01-08 21:17:38 +0000

    finish transformers

--- src/main/java/org/deblock/exercise/transformer/crazyair/CrazyAirTransformer.java
+++ src/main/java/org/deblock/exercise/transformer/crazyair/CrazyAirTransformer.java
@@ -7,15 +7,21 @@ import org.deblock.exercise.model.rest.crazyair.CrazyAirRequest;
 import org.deblock.exercise.model.rest.crazyair.CrazyAirResponse;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneId;
 import java.util.Date;
 
 @Component
 public class CrazyAirTransformer {
 
-    public CrazyAirRequest toCARequest(DeblockRequest dRequest) {
+    public CrazyAirRequest toCARequest(DeblockRequest dReq) {
 
         CrazyAirRequest caReq = new CrazyAirRequest();
 
+        caReq.setOrigin(dReq.getOrigin());
+        caReq.setDestination(dReq.getDestination());
+        caReq.setDepartureDate(dReq.getDepartureDate());
+        caReq.setReturnDate(dReq.getReturnDate());
+        caReq.setPassengerCount(dReq.getNumberOfPassengers());
         return caReq;
 
     }
@@ -23,6 +29,18 @@ public class CrazyAirTransformer {
     public DeblockResponse toDResponse(CrazyAirResponse caResp) {
 
         DeblockResponse dResp = new DeblockResponse();
+
+        //Comment on this... bad!
+        Date depDate = Date.from(caResp.getDepartureDate().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date arrDate = Date.from(caResp.getArrivalDate().atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        dResp.setAirline(caResp.getAirline());
+        dResp.setSupplier("CrazyAir");
+        dResp.setFare(caResp.getPrice());
+        dResp.setDepartureAirportCode(caResp.getDepartureAirportCode());
+        dResp.setDestinationAirportCode(caResp.getDestinationAirportCode());
+        dResp.setDepartureDate(depDate);
+        dResp.setArrivalDate(arrDate);
 
         return dResp;
     }
