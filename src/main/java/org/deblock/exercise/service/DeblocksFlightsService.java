b7d2d79f9908358bb2b247e9603557909ff0178a
JamieW87 <jamie.w101@gmail.com>
2023-01-09 00:15:36 +0000

    change service calls to list

--- src/main/java/org/deblock/exercise/service/DeblocksFlightsService.java
+++ src/main/java/org/deblock/exercise/service/DeblocksFlightsService.java
@@ -3,7 +3,6 @@ package org.deblock.exercise.service;
 import org.deblock.exercise.model.rest.DeblockRequest;
 import org.deblock.exercise.model.rest.DeblockResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,27 +11,56 @@ import java.util.List;
 @Service
 public class DeblocksFlightsService {
 
-    private final SupplierService CAService;
-    private final SupplierService TJService;
+    @Autowired
+    private final List<SupplierService> supplierServices;
 
     @Autowired
-    public DeblocksFlightsService(@Qualifier("crazyair") SupplierService caService,
-                                  @Qualifier("toughjet") SupplierService tjService) {
-        this.CAService = caService;
-        this.TJService = tjService;
+    public DeblocksFlightsService(List<SupplierService> supplierServices) {
+        this.supplierServices = supplierServices;
     }
 
     public DeblockResponse[] FetchFlightData(DeblockRequest deblockRequest) {
 
         List<DeblockResponse> responseList = new ArrayList<>();
 
-        DeblockResponse caResponse = CAService.GetFlightData(deblockRequest);
-        DeblockResponse tjResponse = TJService.GetFlightData(deblockRequest);
-
-        responseList.add(caResponse);
-        responseList.add(tjResponse);
+        supplierServices.forEach(svc -> {
+            DeblockResponse resp = svc.GetFlightData(deblockRequest);
+            responseList.add(resp);
+        });
 
         return responseList.toArray(new DeblockResponse[responseList.size()]);
 
     }
 }
+
+
+//@Service
+//public class DeblocksFlightsService {
+//
+//    private final SupplierService CAService;
+//    private final SupplierService TJService;
+//
+//
+//
+//    @Autowired
+//    public DeblocksFlightsService(@Qualifier("crazyair") SupplierService caService,
+//                                  @Qualifier("toughjet") SupplierService tjService) {
+//        this.CAService = caService;
+//        this.TJService = tjService;
+//    }
+//
+//    public DeblockResponse[] FetchFlightData(DeblockRequest deblockRequest) {
+//
+//        List<DeblockResponse> responseList = new ArrayList<>();
+//
+//        DeblockResponse caResponse = CAService.GetFlightData(deblockRequest);
+//        DeblockResponse tjResponse = TJService.GetFlightData(deblockRequest);
+//
+//
+//        responseList.add(caResponse);
+//        responseList.add(tjResponse);
+//
+//        return responseList.toArray(new DeblockResponse[responseList.size()]);
+//
+//    }
+//}
