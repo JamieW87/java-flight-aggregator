62941bf31c41bddb9d41c451dd4655f657049fba
JamieW87 <jamie.w101@gmail.com>
2023-01-09 21:07:42 +0000

    nearly there

--- src/test/java/org/deblock/exercise/ExerciseApplicationIT.java
+++ src/test/java/org/deblock/exercise/ExerciseApplicationIT.java
@@ -5,16 +5,19 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.deblock.exercise.controller.DeblockFlightsController;
 import org.deblock.exercise.model.rest.DeblockRequest;
 import org.deblock.exercise.model.rest.DeblockResponse;
-import org.deblock.exercise.service.impl.CrazyAirService;
-import org.deblock.exercise.service.impl.ToughJetService;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -22,6 +25,22 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @WireMockTest(httpPort = 8089)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExerciseApplicationIT {
+
+    String mockDeblockReq = "{\n" +
+            "  \"origin\":\"LHR\",\n" +
+            "  \"destination\": \"MIA\",\n" +
+            "  \"departureDate\": \"2024-11-12\",\n" +
+            "  \"returnDate\": \"2023-12-01\",\n" +
+            "  \"numberOfPassengers\":3\n" +
+            "}";
+
+    String mockTJReq = "{\n" +
+            "  \"from\":\"LHR\",\n" +
+            "  \"to\": \"MIA\",\n" +
+            "  \"outboundDate\": [ 2024, 11, 12 ],\n" +
+            "  \"inboundDate\": [ 2023, 12, 11 ],\n" +
+            "  \"numberOfAdults\":3\n" +
+            "}";
 
     @Value("${mock.base.url}")
     String mockBaseUrl;
@@ -33,10 +52,7 @@ public class ExerciseApplicationIT {
     private DeblockResponse deblockResponse;
 
     @Autowired
-    private CrazyAirService crazyAirService;
-
-    @Autowired
-    private ToughJetService toughJetService;
+    private DeblockFlightsController controller;
 
     // **** Wiremock setup Start
     private static WireMockServer wireMockServer;
@@ -67,8 +83,15 @@ public class ExerciseApplicationIT {
     @DisplayName("Tests the successful retrieval of flight data")
     public void testValidResponseBodyReturnsDataFromSuppliers() {
 
+        DeblockRequest debReq = new DeblockRequest();
+        debReq.setOrigin("LHR");
+        debReq.setDestination("MIA");
+        debReq.setDepartureDate(LocalDate.parse("2024-11-12"));
+        debReq.setReturnDate(LocalDate.parse("2023-12-11"));
+        debReq.setNumberOfPassengers(3);
+
         String urlPath = "/api/flights";
-        wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo(urlPath)).withRequestBody()
+        wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo(urlPath)).withRequestBody(equalToJson(mockTJReq))
                 .willReturn(
                         aResponse()
                                 .withStatus(200)
@@ -76,9 +99,9 @@ public class ExerciseApplicationIT {
                                 .withBodyFile("mock-api/deblockResponse.json"))
         );
 
-        DeblockResponse response = crazyAirService.GetFlightData(deblockReq);
+        ResponseEntity response = controller.fetchFlights(debReq);
 
-        //assertThat(response).isNotNull();
+        assertThat(response).isNotNull();
         //assertThat(response).isEqualTo(deblockResponse);
     }
 }
